b325ee0caaf1f33bd0264b1f2a0d822a51fd806d
m4x0n <98maxon98@gmail.com>
2021-01-12 09:49:28 +0200

    Finally it works

--- network.cpp
+++ network.cpp
@@ -1,169 +1,175 @@
+#include "network.h"
+//! Друже, не проеби главную идею: в этой части никаких лишних методов.
+//! Всякие утилиты для поиска клетки на слое и прочие свистелки - в отдельный хедер
+//! Все, что нам доступно для голой сети - добавить в нее нейрон на интересующий нас слой,
+//! добавить синапс на слой и тикнуть сеть.
+//! Дилемма: стоит ли отделить манипуляции со слоями от добавления элементов
+//!
+//! За разделение: прозрачность. Никакого имплицитного трансформирования структуры
+//! За совмещение: не нужно будет отдельной утилитарной функцией выяснять, что где находится
+// todo Вечером после душа принято решение разделять
+//? Вот еще: второй генотипический (дабловый) параметр у синапса свободен. Можно применить для А - коэффициента обучения, Б - коэффициента реаптейка
 
 
+/* // TODO РАСПРЕДЕЛИ ВСЕ В ПОРЯДКЕ
+TABLE OF CONTENTS:
+- NETWORK::SYNTHESIS
+- NETWORK::REFRESHMENT
+-
+- CONSTRUCTORS
+*/
 
-//EASY BUILDER
-// Neuron
-void neuro::Network::addCell(std::string func, double alpha, double bias, unsigned int layer)
+/*	SYNTHESIS	*/
+unsigned int neuro::Network::addNeuron(Activation* func, double alpha, double bias, unsigned int layer)
 {
-    enableNeuron( addNeuron( func, alpha, bias ), layer );
+	_neurons.emplace_back( func, alpha, bias );
+	if( _cellsTable.size() <= layer ) 	addLayer(layer);
+	_cellsTable[layer].emplace_back( &_neurons.back() );
+	return _neurons.size()-1 ;
 }
-// Synaps
-void neuro::Network::addCell(std::string axon, std::string dendron, double weight)
+
+bool neuro::Network::addSynaps( std::string axon, std::string dendron, double weight, unsigned int layer )
 {
-    enableSynaps( addSynaps(axon, dendron, weight), findLayer(_takeAxon(axon))+1 )
+	if(	_addSynaps(axon, dendron, weight) )	{
+		if( _cellsTable.size() <= layer )	addLayer(layer);
+		_cellsTable[layer].emplace_back( &_synapses.back() );
+		return true;
+	} else {
+		return false;
+	}
 }
-
-//FINDER
-int findLayer(Cell* cell)
+bool neuro::Network::addSynaps( std::string axon, unsigned int dendron, double weight, unsigned int layer )
 {
-    for(int i = 0; i < _cellsTable.size(); ++i) {
-        for(int j = 0; j < _cellsTable[i].size(); ++j) {
-            if(cell == _cellsTable[i][j]) return i;
-        }
-    }
-    return -1;    
+	if(	_addSynaps(axon, dendron, weight) )	{
+		if( _cellsTable.size() <= layer )	addLayer(layer);
+		_cellsTable[layer].emplace_back( &_synapses.back() );
+		return true;
+	} else {
+		return false;
+	}
 }
-
-// ALMOST DONE LINE ----------------------------
-
-//ENABLER
-void neuro::Network::_enableCell(Cell* cell, unsigned int layer) {
-    if( _cellsTable.size() <= layer ) { _cellsTable.resize(layer+1); }
-    _cellsTable[layer].push_back( cell );
-}
-void neuro::Network::enableNeuron(unsigned int neuron, unsigned int layer) {
-    if( _neurons.size() > neuron ) {
-        _enableCell( &_neurons[neuron], layer );
-    } else {
-        LOG("enableNeuron, neuron number");LOG(neuron);LOG("not found");
-    }
-}
-void neuro::Network::enableSynaps(unsigned int synaps, unsigned int layer) {
-    if( _synapses.size() > synaps ) {
-        _enableCell( &_synapses[synaps], layer );
-    } else {
-        LOG("enableSynaps, synaps number");LOG(synaps);LOG("not found");
-    }
-}
-
-//DISABLER
-void neuro::Network::disableCell(unsigned int layer, unsigned int cell) {
-    _cellsTable[layer].erase( _cellsTable[layer].begin()+cell );
-}
-
-//LAYER MANIPULATION 
-void neuro::Network::insertLayer(unsigned int layer) {
-    if( _cellsTable.size() > layer ) {
-        _cellsTable.emplace( _cellsTable.begin()+layer );
-    } else {
-        _cellsTable.resize(layer+1);
-    }
-}
-
-//ADDERS
-int neuro::Network::addNeuron(std::string func, double alpha, double bias)
+bool neuro::Network::addSynaps( unsigned int axon, std::string dendron, double weight, unsigned int layer )
 {
-    _neurons.emplace_back( func, alpha, bias );
-    return _neurons.size()-1 ;
+	if(	_addSynaps(axon, dendron, weight) )	{
+		if( _cellsTable.size() <= layer )	addLayer(layer);
+		_cellsTable[layer].emplace_back( &_synapses.back() );
+		return true;
+	} else {
+		return false;
+	}
+}
+bool neuro::Network::addSynaps( unsigned int axon, unsigned int dendron, double weight, unsigned int layer )
+{
+	if(	_addSynaps(axon, dendron, weight) )	{
+		if( _cellsTable.size() <= layer )	addLayer(layer);
+		_cellsTable[layer].emplace_back( &_synapses.back() );
+		return true;
+	} else {
+		return false;
+	}
 }
 
-// takers for creating synapses. Return NULL if not found
+unsigned int neuro::Network::addLayer( unsigned int layer )
+{
+	if( layer < _cellsTable.size() ) { // Insertion case
+		std::vector< Cell* > emptylayer;
+		_cellsTable.insert( _cellsTable.begin() + layer, emptylayer );
+	} else { // extension case
+		_cellsTable.resize( layer+1 );
+	}
+	return _cellsTable.size();
+}
+
+/*	SYNTHESIS AUXILLARY METHODS		*/
+bool neuro::Network::_addSynaps(auto in, auto out, double weight)
+{
+	double* axonptr = _takeAxon(in);
+	double* dendronptr = _takeDendron(out);
+	//Check for null(notfound) returns
+	if( axonptr != nullptr && dendronptr != nullptr ) {
+		_synapses.emplace_back( axonptr, dendronptr, weight );
+		return true;
+	} else {
+		return false;
+	};
+}
+//Overloaded takers. Return NULL if element not found.
 double* neuro::Network::_takeAxon(int neuron) {
-    if( neuron < _neurons.size() ) {
-        return &_neurons[neuron]._axon;
-    } else {
-        LOG("_takeAxon, neuron number"); LOG(neuron); LOG("Not found");
-        return NULL;
-    }
+	if( neuron < _neurons.size() ) {
+		auto i = _neurons.begin();
+		std::advance(i, neuron);
+		return &i->_axon;
+	} else {
+		return nullptr;
+	}
 }
 double* neuro::Network::_takeDendron(int neuron) {
-    if( neuron < _neurons.size() ) {
-        return &_neurons[neuron]._dendron;
-    } else {
-        LOG("_takeDendron, neuron number"); LOG(neuron); LOG("Not found");
-        return NULL;
-    }
+	if( neuron < _neurons.size() ) {
+		auto i = _neurons.begin();
+		std::advance(i, neuron);
+		return &i->_dendron;
+	} else {
+		return nullptr;
+	}
 }
 double* neuro::Network::_takeAxon(std::string input) {
-    if( _inputs.find(input) != _inputs.end() ) {
-        return _inputs[input];
-    } else {
-        LOG("_takeAxon, input"); LOG(input); LOG("Not found");
-        return NULL;
-    }
+	if( _inputs.find(input) != _inputs.end() ) {
+		return _inputs[input];
+	} else {
+		return nullptr;
+	}
 }
 double* neuro::Network::_takeDendron(std::string output) {
-    if( _outputs.find(output) != _outputs.end() ) {
-        return _outputs[output];
-    } else {
-        LOG("_takeDendron, output"); LOG(output); LOG("Not found");
-        return NULL;
-    }
+	if( _outputs.find(output) != _outputs.end() ) {
+		return _outputs[output];
+	} else {
+		return nullptr;
+	}
 }
 
-// synaps creator (called by public addSynaps mask functions, they're in .h)
-// returns index of synaps or -1 if not successful
-int neuro::Network::_addSynaps(auto in, auto out, double weight)
-{
-    double* axonptr = _takeAxon(in);
-    double* dendronptr = _takeDendron(out);
-    //Check for null(notfound) returns
-    if( axonptr != 0 && dendronptr != 0 ) {
-        _synapses.emplace_back( axonptr, dendronptr, weight );
-        return _synapses.size()-1;
-    } else { 
-        LOG("_addSynaps"); LOG(in); LOG(out); LOG("Synaps creation not successful, skipping");
-        return -1;
-    };
-}
-
-//REFRESHES
-void neuro::Synaps::refresh() 
+/*	REFRESHES	*/
+void neuro::Synaps::refresh()
 {
 	*_dendronptr += *_axonptr * _weight;
 }
 
-void neuro::Neuron::refresh() 
+void neuro::Neuron::refresh()
 {
-	if( activations.find(_function) == activations.end() ) {
-		MSG("neuron: FUNCTION not found in Functions.h");
-        ENDMSG();
-		exit(1);
-	} else {	
-		_axon = activations[_function](_dendron+_bias,_alpha);	
-		_dendron = 0;
+	_axon = (*_function)(_dendron+_bias,_alpha);
+	_dendron = 0;
+}
+
+void neuro::Network::tick()
+{
+	for(auto const& layer : _cellsTable) {
+		for(auto const& cell : layer) {
+			cell->refresh();
+		}
 	}
 }
 
-void neuro::Network::tick() 
-{
-    for(auto&& layer : _cellsTable) {
-        for(auto&& cell : layer) {
-            cell->refresh();
-        }
-    }
-}
-
-//CONSTRUCTORS
+/*	CONSTRUCTORS	*/
 neuro::Network::Network(iomap inputs, iomap outputs)
 {
-    _inputs = inputs;
-    _outputs = outputs;
+	_inputs = inputs;
+	_outputs = outputs;
 }
 
-neuro::Neuron::Neuron(std::string func, double alpha, double bias)
+neuro::Neuron::Neuron(Activation* func, double alpha, double bias)
 {
-    _axon = 0;
-    _function = func;
-    _alpha = alpha;
-    _bias = bias;
-    _dendron = 0;
-    refresh();
+	_axon = 0;
+	_function = func;
+	_alpha = alpha;
+	_bias = bias;
+	_dendron = 0;
+	
+	refresh();
 }
 
 neuro::Synaps::Synaps(double* axon, double* dendron, double weight)
 {
-    _axonptr = axon;
-    _dendronptr = dendron;
-    _weight = weight;
+	_axonptr = axon;
+	_dendronptr = dendron;
+	_weight = weight;
+	refresh();
 }
--- network.h
+++ network.h
@@ -1,12 +1,10 @@
-//#pragma once
+#pragma once
 
 /*
 This part of library implements basic level of creating ANN of practically any traditional type
 
 Class Network includes methods for building:
-    Adding elements (addNeuron, addSynaps)
-    Setting order of activation (enableNeuron, enableSynaps)
-    Insertion of layer (insertLayer) under layer or on top
+addNeuron, addSynaps, addLayer
 
 And methods, that return references to vectors of neurons, synapses and layers.
 */
@@ -15,10 +13,15 @@ And methods, that return references to vectors of neurons, synapses and layers.
 #include<string>
 #include<vector>
 #include<map>
-#include<utility>
+#include<list>
 
 #include"functions.h"
 
+
+#define LOG(msg) {std::cout << msg << std::endl;}
+
+/*
+//TODO переделай эту хуйню
 #define MSG(msg) {std::cout << msg << "\t";} 
 #define ENDMSG() {std::cout << std::endl;}
 
@@ -27,123 +30,110 @@ And methods, that return references to vectors of neurons, synapses and layers.
 //#else
 //#define LOG(msg) {}
 //#endif
+//////// */
 
 namespace neuro 
 {
 	using iomap = std::map< std::string, double* >;
 
-    class Cell 
-    {   public: 
-            void virtual refresh() = 0; 
-    };
+	class Cell 
+	{ 
+	friend class Network;
 
-    class Neuron : public Cell
+	public: 
+	void virtual refresh() = 0; 
+	};
+	
+	class Neuron : public Cell
 	{
 	protected:      		
-		std::string _function;
+		Activation* _function;
 		double _alpha;
-        double _bias;
+		double _bias;
 		double _axon;
 		double _dendron;
-
-        friend class Network;
-        friend class Cell;
-	public:
 		
-        // Only for external use
-		const std::string& 	getFunction()	const	{ return _function; }
-		const double& 		getAlpha() 		const	{ return _alpha; }
-        const double& 		getBias() 		const	{ return _bias; }
-		const double& 		getAxon() 		const	{ return _axon; }
-		const double& 		getDendron()	const	{ return _dendron; }
+		friend class Network;
+		friend class Cell;
+	public:
+//	Useless now
+//		const Activation&	getFunction()	const	{ return _function; }
+//		const double& 		getAlpha() 		const	{ return _alpha; }
+//		const double& 		getBias() 		const	{ return _bias; }
+//		const double& 		getAxon() 		const	{ return _axon; }
+//		const double& 		getDendron()	const	{ return _dendron; }
+		
+		explicit Neuron(Activation* func, double alpha, double bias);
 
-        explicit Neuron(std::string func, double alpha, double bias);
 		void refresh() override;
-	}; 
-    
-    class Synaps : public Cell
+	};
+	
+	class Synaps : public Cell
 	{
 	protected:
 		double* _axonptr;
 		double* _dendronptr;
 		double _weight;
-
-        friend class Network;
-        friend class Cell;
+		
+	friend class Network;
+	friend class Cell;
 	public:
-
-        // Only for external use
-		const double& 		getWeight()		const	{ return _weight; }
-        // For any use
-		const double* 		getAxon() 		const	{ return _axonptr; }
-		const double* 		getDendron() 	const	{ return _dendronptr; }
-
-        explicit Synaps(double* axon, double* dendron, double weight);
-
+		
+		// Not very useful
+		/*
+		const double&	getWeight()	const	{ return _weight; }
+		const double*	getAxon() 	const	{ return _axonptr; }
+		const double*	getDendron()	const	{ return _dendronptr; }
+		*/
+	
+		explicit Synaps(double* axon, double* dendron, double weight);
+			
 		void refresh() override;
 	};
-
-	using Neurons = std::vector<Neuron>;
-	using Synapses = std::vector<Synaps>;
-    
-    using CellsTable = std::vector< std::vector< Cell* > >;
-
+	
+	using Neurons = std::list<Neuron>;
+	using Synapses = std::list<Synaps>;
+	using CellsTable = std::vector< std::vector< Cell* > >;
+	
 	class Network
 	{
 	protected:
-        iomap _inputs;
-        iomap _outputs;
-
+		iomap _inputs;
+		iomap _outputs;
+		
 		Neurons _neurons;
 		Synapses _synapses;	
-        std::vector< std::vector< Cell* > > _cellsTable;
+		CellsTable _cellsTable;
+		
+		double* _takeAxon(int neuron);
+		double* _takeAxon(std::string input);
+		double* _takeDendron(int neuron);
+		double* _takeDendron(std::string output);
 
-        // Taking elements (nonconstant pointers to them)
-        // Retrun NULL if element doesnt exist
-        double* _takeAxon(int neuron);
-        double* _takeAxon(std::string input);
-        double* _takeDendron(int neuron);
-        double* _takeDendron(std::string output);
-        
-        int _addSynaps(auto in, auto out, double weight); 
-
-        //adding cells to activation order
-        void _enableCell(Cell* cell, unsigned int layer);
-
+		bool _addSynaps( auto in, auto out, double weight );
+		
 	public:	
 		const Neurons&      getNeurons() 	    { return _neurons; }
 		const Synapses&     getSynapses() 	    { return _synapses; }
-        const CellsTable&   getCellsTable()     { return _cellsTable; }
-    
-    //EZ building methods
-    // addCell returns true if adding is successful
-        void addCell(std::string, double, double, unsigned int);
-        void addCell(std::string, std::string, double);
-
-    //"Surgical" building methods
-    //STRUCTURAL MODIFICATIONS
-        int addNeuron( std::string func, double alpha, double bias ); // returns index
-        // "Mask" overloaded method for private _addSynaps
-        // Returns index of newly created synaps in _synapses or -1,
-        int addSynaps(std::string in, std::string out, double weight)     { return _addSynaps(in,out,weight); }
-        int addSynaps(unsigned int in, std::string out, double weight)    { return _addSynaps(in,out,weight); }
-        int addSynaps(std::string in, unsigned int out, double weight)    { return _addSynaps(in,out,weight); }
-        int addSynaps(unsigned int in, unsigned int out, double weight)   { return _addSynaps(in,out,weight); }
-
-        // Adding cells to activation order. Without it cell will never be activated.
-        void enableNeuron(unsigned int neuron, unsigned int layer);
-        void enableSynaps(unsigned int synaps, unsigned int layer);
-        // Remove cell from activation order
-        void disableCell(unsigned int layer, unsigned int cell);
-        // Auxillary function for insertion layer under layer n. If there are no layer n - adding layers up to n on top;
-        void insertLayer(unsigned int layer);
-
-    // INTERNAL MODIFICATIONS
-        void adjustAlpha(unsigned int neuron, double alpha);
-        void adjustBias(unsigned int neuron, double bias);
-        void adjustWeight(unsigned int synaps, double weight);
-
-		explicit Network(iomap inputs, iomap outputs);
+		const CellsTable&   getCellsTable()     { return _cellsTable; }
+	//REFRESHMENT
 		void tick();
+	//SYNTHESIS
+		//Add neuron to Layer and return it's index from _neurons. Will call addLayer if layer not exists.
+		unsigned int addNeuron( std::function<double(double,double)>* , double alpha, double bias, unsigned int layer );
+		
+		// Add synaps from axon to dendron, place it to layer. Returns if successful or not (A or D not found). If layer not exists - addLayer called.
+		bool addSynaps( std::string     axon,  std::string  dendron, double weight, unsigned int layer);
+		bool addSynaps( std::string     axon,  unsigned int dendron, double weight, unsigned int layer);
+		bool addSynaps( unsigned int    axon,  std::string  dendron, double weight, unsigned int layer);
+		bool addSynaps( unsigned int    axon,  unsigned int dendron, double weight, unsigned int layer);
+		// Add pseudo-synaps with weight 1 that sends state of axon to external variable. True if succeed
+		bool addProbe( unsigned int axon, double* output, unsigned int layer );
+		
+		// Add empty layerr. If layer exists - new will be unserter under. If not - net will be expanded. Returns new number of layers.
+		unsigned int addLayer(unsigned int layer); 
+		
+	//CONSTRUCTOR
+		explicit Network(iomap inputs, iomap outputs);
 	};	
 }
